642655a574854313101c27a9d4c8c04eb7101682
lgucebu1 <lgucebu1@lgucebu1@ramesessystems.org>
2023-09-04 12:56:15 +0800

    Second commit

--- Backup/04-backup-complete.sh
+++ Backup/04-backup-complete.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+
+#Get the etracs/bin folder
+etracs_bin=docker/bin
+
 #Get the current date
 date_now=$(date +'%Y-%b-%d')
 
 #Maximum backup files in MySQL folder
 max_files=1
 
-#Maintaining three compressed files in the folder by removing the oldest and creating the newest
+#Maintaining max number of compressed files in the folder by removing the oldest and creating the newest
 cd MySQL
 while [ "$(ls -1 | wc -l)" -ge "$max_files" ]; do
      oldest_file=$(ls -1tr | head -1)
@@ -27,17 +31,16 @@ echo ""
 rm -rf $date_now
 
 #Go to docker/bin
-cd
-cd docker/bin
+cd ~/etracs_bin
 
-#Start the Etracs server to allow transactions
+# Pause first then start the Etracs server to allow transactions
+sleep 3
 echo "Starting Etracs Server"
-echo ""
 sh start.sh
 echo ""
 
-#Start the GDX server to allow online transactions
+#Pause first then Start the GDX server to allow online transactions
+sleep 3
 echo "Starting GDX Server"
-echo ""
 sh restart-gdx.sh
-exit
+echo ""
